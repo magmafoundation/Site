a91ce946ac1629a4b50ee75eb7e10320be9c5e3d
Hexeption <minecraftfun201@gmail.com>
2021-07-07 20:34:53 +0100

    Removed Footer Links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-scroll';
-
 export function Footer() {
 
     return (
@@ -14,25 +12,6 @@ export function Footer() {
             <div className='container'>
                 <div className='footer-logo text-center'>
                     <img src='images/footer.png' className='attachment-large size-large' alt='' />
-                </div>
-                <div className='footer-menu'>
-                    <ul id='menu-footer-menu' className='menu'>
-                        <li>
-                            <Link to='/'><i className='fas fa-home' /> <a>Home</a></Link>
-                        </li>
-                        <li>
-                            <Link smooth={true} duration={500} to='about'><i
-                                className='fas fa-info' /> <a>About</a></Link>
-                        </li>
-                        <li>
-                            <Link smooth={true} duration={500} to='partners'><i
-                                className='fas fa-handshake' /> <a>Partners</a></Link>
-                        </li>
-                        <li>
-                            <Link smooth={true} duration={500} to='downloads'><i
-                                className='fas fa-download' /> <a>Downloads</a></Link>
-                        </li>
-                    </ul>
                 </div>
                 <p className='copyright text-center'>© 2018 - 2021 Magma Development</p>
                 <p className='copyright text-center'>Registered in United Kingdom ID: #12428426</p>
